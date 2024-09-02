05f06fc097d80352afa07d8e376ed1cf102a22b1
Jitesh Kumar <150531216+JITESH-KUMAR05@users.noreply.github.com>
2024-09-02 09:58:53 +0000

    added pages

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
 import Image from "next/image";
+import Hero from "./hero/page";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
+      <Hero />
+      <footer />
+      
       
     </div>
   );
